8c5763c29130eb59fde29a5dfead58387b3b7c00
vlaurenzano <vincent.laurenzano@gmail.com>
2020-10-10 18:18:11 -0400

    initial commit in clean repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# cds-service-example-python
+
+Simple python CDS service using the `cds-hooks-work` framework. 
+
+View it in action here: 
+
+https://sandbox.cds-hooks.org/?serviceDiscoveryURL=https://test-cds-service.herokuapp.com/cds-services
+
---
+++ app.py
@@ -0,0 +1,16 @@
+import cds_hooks_work as cds
+import os
+
+app = cds.App()
+
+
+@app.patient_view("hello-world", "The patient greeting service greets a patient!", title="Patient Greeter")
+def greeting(r: cds.PatientViewRequest, response: cds.Response) -> cds.Response:
+    response.cards = [cds.Card.info("hello world!", "demo_service")]
+    response.httpStatusCode = 200
+
+
+if __name__ == '__main__':
+    debug = os.environ.get('DEBUG', False)
+    port = os.environ.get("PORT", 5000)
+    app.serv(host="0.0.0.0", debug=debug, port=port)
