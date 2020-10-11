ae9f6e8c63d10a01a5c0d469fa092068424d51ee
vlaurenzano <vincent.laurenzano@gmail.com>
2020-10-11 13:24:10 -0400

    update to reflect changes in library

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ app = cds.App()
 
 
 @app.patient_view("hello-world", "The patient greeting service greets a patient!", title="Patient Greeter")
-def greeting(r: cds.PatientViewRequest, response: cds.Response) -> cds.Response:
-    response.cards = [cds.Card.info("hello world!", "demo_service")]
+def greeting(r: cds.PatientViewRequest, response: cds.Response):
+    card = cds.Card.info("hello world!", cds.Source("demo_service"))
+    card.add_link(cds.Link.absolute("cds-hooks-work", "https://github.com/vlaurenzano/cds-hooks-work"))
+    response.add_card(card)
     response.httpStatusCode = 200
 
 
 if __name__ == '__main__':
     debug = os.environ.get('DEBUG', False)
     port = os.environ.get("PORT", 5000)
-    app.serv(host="0.0.0.0", debug=debug, port=port)
+    app.serve(host="0.0.0.0", debug=debug, port=port)
